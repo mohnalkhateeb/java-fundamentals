ad694a8e096d26dda2ca04ad0e71e5c227af4915
mohnalkhateeb <mhmmd.alkateeb@gmail.com>
2021-09-28 23:47:40 +0300

    lab03

--- basiclibrary/lib/src/main/java/basiclibrary/Main.java
+++ basiclibrary/lib/src/main/java/basiclibrary/Main.java
@@ -1,6 +1,8 @@
 package basiclibrary;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
@@ -22,6 +24,27 @@ public class Main {
                 {65, 56, 55, 52, 55, 62, 57},
         }))) ;
 
+        //Lab3 call functions
+        System.out.println((demo.temperaturesHashSet(new int[][]{
+                {66, 64, 58, 65, 71, 57, 60},
+                {57, 65, 65, 70, 72, 65, 51},
+                {55, 54, 60, 53, 59, 57, 61},
+                {65, 56, 55, 52, 55, 62, 57},
+        }))) ;
+
+
+        List<String> votes = new ArrayList<>();
+        votes.add("Bush");
+        votes.add("Bush");
+        votes.add("Bush");
+        votes.add("Shrub");
+        votes.add("Hedge");
+        votes.add("Shrub");
+        votes.add("Bush");
+        votes.add("Hedge");
+        votes.add("Bush");
+
+        System.out.println(demo.tally(votes));
 
     }
 }
--- basiclibrary/lib/src/test/java/basiclibrary/LibraryTest.java
+++ basiclibrary/lib/src/test/java/basiclibrary/LibraryTest.java
@@ -5,6 +5,9 @@ package basiclibrary;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -62,5 +65,41 @@ class LibraryTest {
 
 
         assertEquals(expectedOutput, testLib.arrayOfArrays(weeklyMonthTemperatures));
+    }
+
+    // lab3 test
+    @Test public void temperaturesHashSetTest (){
+        int[][] weeklyMonthTemperatures = {
+                {66, 64, 58, 65, 71, 57, 60},
+                {57, 65, 65, 70, 72, 65, 51},
+                {55, 54, 60, 53, 59, 57, 61},
+                {65, 56, 55, 52, 55, 62, 57}
+        };
+
+        String outputShould =
+                "High: 72\n" +
+                        "Low: 51\n" +
+                        "Never saw temperature: 63\n" +
+                        "Never saw temperature: 67\n" +
+                        "Never saw temperature: 68\n" +
+                        "Never saw temperature: 69\n";
+
+        assertEquals(outputShould, testLib.temperaturesHashSet(weeklyMonthTemperatures));
+    }
+    @Test public void testTally(){
+        List<String> votes = new ArrayList<>();
+        votes.add("Bush");
+        votes.add("Bush");
+        votes.add("Bush");
+        votes.add("Shrub");
+        votes.add("Hedge");
+        votes.add("Shrub");
+        votes.add("Bush");
+        votes.add("Hedge");
+        votes.add("Bush");
+
+        String expectedOutput = "Bush received the most votes!";
+
+        assertEquals(expectedOutput, testLib.tally(votes));
     }
 }
